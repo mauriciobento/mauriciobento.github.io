8448937d27fadcf11cc4a7f2a2f1c1101ff86a90
P2708165 <c-mauricio.bento@charter.com>
2020-02-28 07:21:45 -0700

    week 07: Lazy Load 07

---
+++ galery07/js/lazyLoad.js
@@ -0,0 +1,28 @@
+const images = document.querySelectorAll("[data-src]");
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 300px 0px"
+};
+const imgObserver = new IntersectionObserver((entries,
+                                              imgObserver) =>{
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        }else{
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src = src;
+}
+
+images.forEach(image => {
+   imgObserver.observe(image)
+});
